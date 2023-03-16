10bd91a24d44c57b5111772d65825574464d95e7
AleksanderMukhin <mukhin.aleksander.ua@gmail.com>
2023-03-16 22:20:26 +0100

    fix task 3 saveData

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,12 +3,12 @@ import throttle from "lodash.throttle";
 const onForm = document.querySelector('.feedback-form');
 const email = document.querySelector('input');
 const textarea = document.querySelector('textarea');
-const STORAG_KEY = 'feedback-form-state'
+const STORAGE_KEY = 'feedback-form-state'
 
 onForm.addEventListener('submit', onFormSubmit);
 onForm.addEventListener('input', throttle(onFormInput, 1000));
 
-const formData = {};
+let formData = {};
 
 populateTextarea();
 
@@ -24,28 +24,33 @@ function onFormSubmit(evt) {
   evt.currentTarget.reset();
 
   // выводим в консоль
-  console.log(JSON.parse(localStorage.getItem(STORAG_KEY)));
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
 
   // чистим хранилище
-  localStorage.removeItem(STORAG_KEY);
+  localStorage.removeItem(STORAGE_KEY);
 };
 
 // В объект форм дата добавляет ключ с названием поля и  
 // присваивает ему значение, то что водится в это поле
 function onFormInput(evt) {
   formData[evt.target.name] = evt.target.value;
-  localStorage.setItem(STORAG_KEY, JSON.stringify(formData));
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  if (evt.target.name === 'email') {
+    localStorage.setItem('email', evt.target.value);
+  }
 };
 
 // Сохраняет введённый текст и вставляет после перезагрузки страницы
 function populateTextarea() {
-  const savedMessage = localStorage.getItem(STORAG_KEY);
-  const savedData = JSON.parse(savedMessage);
-
-  //Если поле не пустое, то есть строка не пустая тогда true и  
-  // данные из хранилища вносится в пoля месседж e - mail
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
   if (savedMessage) {
-    textarea.value = savedData.message;
-    email.value = savedData.email;
+    const savedData = JSON.parse(savedMessage);
+    if (savedData.email) {
+      email.value = savedData.email;
+    }
+    if (savedData.message) {
+      textarea.value = savedData.message;
+    }
+    formData = savedData;
   }
 };
