457022c16f862fdbe4985291a977483361d3b74c
AleksanderMukhin <mukhin.aleksander.ua@gmail.com>
2023-03-16 19:52:54 +0100

    fix task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,6 +4,14 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 
+// при першому завантаженні у нас немає збережених секунд тому додаємо 
+// у сет Каррент Тайм або те що є у стореджі або 0 ==================
+const currentTime = localStorage.getItem('videoplayer-current-time');
+if (!currentTime) {
+  localStorage.setItem('videoplayer-current-time', 0);
+};
+//=============================================
+
 player.on('timeupdate', throttle(onPlay, 1000));
 
 function onPlay({ seconds }) {
